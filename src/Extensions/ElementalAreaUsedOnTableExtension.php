820128c8ee4ae0f84a666efb533a8888ebec7423
Steve Boyd <emteknetnz@gmail.com>
2020-08-25 15:32:47 +1200

    FIX Exclude ElementalAreas from file used-on-table

--- src/Extensions/ElementalAreaUsedOnTableExtension.php
+++ src/Extensions/ElementalAreaUsedOnTableExtension.php
@@ -3,14 +3,15 @@
 namespace DNADesign\Elemental\Extensions;
 
 use SilverStripe\Admin\Forms\UsedOnTable;
+use SilverStripe\Core\ClassInfo;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\DataObject;
 use DNADesign\Elemental\Models\ElementalArea;
+use DNADesign\Elemental\Models\BaseElement;
 
 class ElementalAreaUsedOnTableExtension extends DataExtension
 {
-
     /**
      * Hides ElementalArea's from the "Used On" tab when viewing files
      *
@@ -21,6 +22,24 @@ class ElementalAreaUsedOnTableExtension extends DataExtension
      */
     public function updateUsage(ArrayList &$dataObjects, DataObject &$record)
     {
-        $usage = $dataObjects->exclude('ClassName', ElementalArea::class);
+        $dataObjects = $dataObjects->exclude('ClassName', ElementalArea::class);
+    }
+
+    /**
+     * Link BaseElement's to their parent page
+     *
+     * @param array $arr
+     * @param DataObject $dataObject
+     * @throws \ReflectionException
+     */
+    public function updateUsageArray(array &$arr, DataObject $dataObject)
+    {
+        $subClasses = ClassInfo::subclassesFor(BaseElement::class);
+        if (!in_array(get_class($dataObject), $subClasses)) {
+            return;
+        }
+        if ($page = $dataObject->getPage()) {
+            $arr['ancestorPageID'] = $page->ID;
+        }
     }
 }
